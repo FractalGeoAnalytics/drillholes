30e71d4d5ecc8cc37f3d4fee53536c5c8cc6f895
Ben Chi <ben@fractalgeoanalytics.com>
2025-11-11 21:17:26 +0800

    formatting etc

--- src/drillhole.py
+++ src/drillhole.py
@@ -4,9 +4,9 @@ import numpy as np
 import wellpathpy as wp
 from dataclasses import dataclass, field
 from typing import Union, Literal, Any
-from datetime import datetime
 from geodata import PointData, IntervalData
 import pyvista as pv
+from dipconversion import strikedip2vector
 
 DFNone = Union[pd.DataFrame, None]
 from tqdm import tqdm
@@ -65,6 +65,13 @@ class Drillhole:
 
     geophysics: pd.DataFrame
         geophysics (wireline logging)
+        assumed to be point data on import then converted to interval data internally when compositing is required.
+        requires a depth column
+
+    struct: pd.DataFrame
+        structural data
+        point data columns need to be dip and dip direction
+
 
     """
 
@@ -83,7 +90,9 @@ class Drillhole:
     strat: DFNone = None
     strat_simplify: Union[dict[str, dict[str, Any]], None] = None
     geophysics: DFNone = None
+    struct: DFNone = None
     watertable: Union[float, None] = None
+    negative_down = (False,)
     display_resolution: float = 0.1
     desurvey_method: Literal[
         "mininum_curvature",
@@ -93,7 +102,6 @@ class Drillhole:
         "high_tangent",
         "low_tangent",
     ] = "mininum_curvature"
-    positive_down: bool = True
 
     def _desurvey(self):
         """
@@ -155,7 +163,8 @@ class Drillhole:
         # add the easting ,northing and elevation create xyz for each point
         self.x = east + self.easting
         self.y = north + self.northing
-        self.z = depth + self.elevation
+        # yes subtract the depth
+        self.z = self.elevation - depth
         if hasSurvey:
             self.survey_type = "collar"
         else:
@@ -173,6 +182,16 @@ class Drillhole:
             )
 
             self._validate_dipazi(self.survey.inclination, self.survey.azimuth)
+
+    def _check_struct(self):
+        """
+        ensure that the struct dataframe contains depth, dip and dipdirection
+        """
+        if isinstance(self.struct, PointData):
+            # survey columns
+            self.struct = PointData(
+                self.struct, extra_validation_columns=["dip", "dipdirection"]
+            )
 
     def _check_stratigraphy(self):
         """
@@ -275,22 +294,67 @@ class Drillhole:
         xyz = np.hstack([f, t]).reshape(-1, 3)
         # look for all the extra columns and return them
         outcolumns = table.columns[~table.columns.isin(["depthfrom", "depthto"])]
-        intervals = table[outcolumns].apply(np.repeat, axis=0, repeats=2)
-
+        # the below line commented out is for when I was plotting as point data
+        # rather than cell data
+        # intervals = table[outcolumns].apply(np.repeat, axis=0, repeats=2)
+        intervals = table[outcolumns]
         return xyz, intervals
+
+    def _point_vtk_generator(self, table):
+        """
+        creates intervals data to pass to vtk
+        """
+        fx, fy, fz = self._interp_survey_depth(table["depth"])
+        xyz = np.vstack((fx, fy, fz)).T
+        # look for all the extra columns and return them
+        outcolumns = table.columns[~table.columns.isin(["depth"])]
+        intervals = table[outcolumns]
+        return xyz, intervals
+
+    def extract_vtk_data(self):
+        """
+        extracts the vtk information to create vtk data but does not create the vtk structures
+        as running vtk creation is very slow
+        """
+        out = {}  # list to store the vtk tmp
+        for i in self.type_map.keys():
+            if i != "survey":
+                tmp = getattr(self, i)
+                if tmp is not None:
+                    if isinstance(tmp, IntervalData):
+                        xyz, intervals = self._interval_vtk_generator(tmp)
+                        dtype = "intervals"
+                    elif isinstance(tmp, PointData):
+                        xyz, intervals = self._point_vtk_generator(tmp)
+                        dtype = "points"
+                    out.update(
+                        {i: {"datatype": dtype, "points": xyz, "data": intervals}}
+                    )
+        return out
 
     def create_vtk(self):
         """
         creates the vtk drillhole datasets for visualisation
         specifically it generates lines that can be displayed correctly in paraview etc.
         """
+        out = {}  # list to store the vtk tmp
         for i in self.type_map.keys():
-            if i != 'survey':
+            if i != "survey":
                 tmp = getattr(self, i)
                 if tmp is not None:
-                    xyz, intervals = self._interval_vtk_generator(tmp)
+                    if isinstance(tmp, IntervalData):
+                        xyz, intervals = self._interval_vtk_generator(tmp)
+                        ll = pv.line_segments_from_points(xyz)
+                        for c in intervals.columns:
+                            ll.cell_data[c] = intervals[c]
 
-        return xyz, intervals
+                    elif isinstance(tmp, PointData):
+                        xyz, intervals = self._point_vtk_generator(tmp)
+                        ll = pv.PointSet(xyz)
+                        for c in intervals.columns:
+                            ll.point_data[c] = intervals[c]
+                    out.update({i: ll})
+        return out
 
     def _desurvey_strat(self):
         """
@@ -353,8 +417,10 @@ class Drillhole:
         # we handle this by setting the dip to 179.99999 avoid the errors probably this is acceptable.
         # the other probably better way is to have negative depths so that a vertical hole goes up
         # but that has a lot of downstream issues as you have to flip a heap of parameter.
+        dip = dip.copy()
         if isinstance(dip, (float, int)):
-            dip = self.__magic_dip
+            if dip == 180:
+                dip = self.__magic_dip
         else:
             dip[dip == 180] = self.__magic_dip
         return dip
@@ -416,6 +482,7 @@ class Drillhole:
             "strat": IntervalData,
             "geophysics": PointData,
             "watertable": PointData,
+            "struct": PointData,
         }
         self.type_map = type_map
         for i in type_map:
@@ -443,16 +510,16 @@ class Drillhole:
         for i in python_basetypes:
             tmp = self.__convert_pd_to_pythontypes(getattr(self, i), i)
             setattr(self, i, tmp)
-        if self.positive_down:
+        # if negative down
+        if self.negative_down:
             self.inclination = 90 + self.inclination
-            if isinstance(self.survey, pd.DataFrame):
+            if isinstance(self.survey, PointData):
                 self.survey.inclination = self._dip_check(90 + self.survey.inclination)
         # check dip and azi
         self._validate_dipazi(self.inclination, self.azimuth)
-
-        self.inclination = self._dip_check(self.inclination)
-        if isinstance(self.survey, pd.DataFrame):
+        if isinstance(self.survey, PointData):
             self.inclination = self._dip_check(self.survey.inclination)
+
         # check hole depth
         if self.depth <= 0:
             raise ValueError("Hole Depth must be >0")
@@ -463,6 +530,7 @@ class Drillhole:
         self._desurvey_assays()
         self._check_stratigraphy()
         self._create_contacts()
+        self._check_struct()
         self._desurvey_strat()
 
         return self
@@ -483,13 +551,16 @@ class DrillData:
     """
 
     collar: pd.DataFrame
-    survey: pd.DataFrame
-    assay: pd.DataFrame
-    strat: pd.DataFrame
-    lith: pd.DataFrame
-    stratcolumn = None
-    wireline: pd.DataFrame
-    desurvey_method: Literal[   
+    survey: pd.DataFrame = None
+    assay: pd.DataFrame = None
+    geology: pd.DataFrame = None
+    strat: pd.DataFrame = None
+    lith: pd.DataFrame = None
+    geophysics: pd.DataFrame = None
+    watertable: pd.DataFrame = None
+    struct: pd.DataFrame = None
+    stratcolumn: pd.DataFrame = None
+    desurvey_method: Literal[
         "mininum_curvature",
         "radius_curvature",
         "average_tangent",
@@ -497,45 +568,93 @@ class DrillData:
         "high_tangent",
         "low_tangent",
     ] = "mininum_curvature"
-    positive_down: bool = True
+    negative_down: bool = True
+
+    def _extract_hole(self, table_name, holeid):
+        """
+        extracts the individual drill holes from a DataFrame
+        """
+
+        tmp = getattr(self, table_name)
+        idx = tmp.holeid == holeid
+        output = tmp[idx].reset_index(drop=True).copy()
+        return output
 
     def __post_init__(self):
+        """
+        creates the dataset
+        """
         drillholes: list[Drillhole] = []
+        # some pre-calculations for speed
+        # check for empty/non-existant tables
+        full_tables: list = []
+        table_names = [
+            "survey",
+            "assay",
+            "geology",
+            "strat",
+            "geophysics",
+            "watertable",
+            "struct",
+        ]
+        for i in table_names:
+            if isinstance(getattr(self, i), pd.DataFrame):
+                full_tables.append(i)
+        # for tables that have information we get the unique bhid's
+        # so that we can only execute the indexing if we need to
+        table_bhids: dict[str, set] = {}
+        for f in full_tables:
+            tmp = getattr(self, f)
+            table_bhids.update({f: set(tmp.holeid.unique().tolist())})
 
-        for i in tqdm(self.collars.HOLEID):
-            cidx = self.collars.HOLEID == i
-            sidx = self.surveys.HOLEID == i
-            aidx = self.assay.HOLEID == i
-            lidx = self.lithology.holeid == i
-            gidx = self.geology.holeid == i
-            tmp_survey = self.surveys.loc[sidx, ["DEPTH", "DIP", "AZIMUTH"]].copy()
-            tmp_survey.columns = ["depth", "inclination", "azimuth"]
+        ubhid = self.collar.holeid.unique()
+        for bhid in tqdm(ubhid):
+            tmp_tables = {}
+            for t in full_tables:
+                if bhid in table_bhids[t]:
+                    tmp_data = self._extract_hole(t, bhid)
+                else:
+                    tmp_data = None
+                tmp_tables.update({t: tmp_data})
 
-            tmp_collar = self.collars[cidx].copy()
-            tmp_assay = self.assay[aidx].copy()
-            tmp_lith = self.lithology[lidx]
-            tmp_geol = self.geology[gidx]
+            cidx = self.collar.holeid == bhid
+            tmp_collar = self.collar[cidx]
             tmp = Drillhole(
-                i,
-                tmp_collar.DEPTH.item(),
-                0,
-                0,
-                tmp_collar.EAST.item(),
-                tmp_collar.NORTH.item(),
-                tmp_collar.RL,
-                survey=tmp_survey,
-                assay=tmp_assay,
-                strat=tmp_lith,
-                geology=tmp_geol,
+                bhid,
+                tmp_collar.depth.item(),
+                tmp_collar.inclination.item(),
+                tmp_collar.azimuth.item(),
+                tmp_collar.easting.item(),
+                tmp_collar.northing.item(),
+                tmp_collar.elevation,
+                **tmp_tables,
             )
+            tmp._contacts
             drillholes.append(tmp)
         self.drillholes = drillholes
 
-    def to_vtk():
+    def to_vtk(self):
         """
         creates a vtk multiblock dataset
         """
-        pass
+        tables = ["assay", "geology", "strat", "geophysics", "watertable", "struct"]
+
+        bins = {v: {} for v in tables}
+        for dh in self.drillholes:
+            # loop over each drillhole and create the vtk datasets
+            vtk_dict = dh.create_vtk()
+            for v in vtk_dict:
+                bins[v].update({dh.bhid: vtk_dict[v]})
+        multiblocks = {}
+        for i in tables:
+            tmpbins = bins[i]
+            if len(tmpbins) > 0:
+                mb = pv.MultiBlock(tmpbins)
+                mb = mb.as_polydata_blocks()
+                for n, name in enumerate(tmpbins.keys()):
+                    mb.set_block_name(n, name)
+                multiblocks.update({i: mb})
+        return multiblocks
 
     def to_csv():
         """
@@ -543,14 +662,110 @@ class DrillData:
         """
         pass
 
-    def to_loop():
+    def to_loop(
+        self, fractional_depth=True, estimate_stratcolumn=True, use_graph_estimate=True
+    ):
         """
-        loop data
+        generates loop data
+        """
+        tmp_contacts = []
+        for dh in self.drillholes:
+            if isinstance(dh.strat, IntervalData):
+                tmp = dh.contacts.copy()
+                tmp["bhid"] = dh.bhid
+                tmp_contacts.append(tmp)
+
+        contacts = pd.concat(tmp_contacts)
+        # estimate the stratigraphic column from stratigraphic transitions
+        if estimate_stratcolumn:
+            import networkx as nx
+
+            cidx = contacts["type"] == "inside"
+
+            units = contacts[cidx]["feature_name"].unique()
+            unit_map = {u[1]: u[0] for u in enumerate(units) if not (u[1] == None)}
+            back_map = {u[0]: u[1] for u in enumerate(units) if not (u[1] == None)}
+            # attempt to estimate the stratigraphic sequence
+            # this is sort of like a transition matrix
+            nunits = len(unit_map)
+            strat_mat = np.zeros([nunits] * 2)
+            t = tmp_contacts[0]
+            for t in tmp_contacts:
+                tidx = t["type"] == "inside"
+                if (~t[tidx].feature_name.isna()).all():
+                    tmp_seq = t[tidx].feature_name.map(unit_map).values
+                    for i in range(len(tmp_seq) - 1):
+                        strat_mat[tmp_seq[i], tmp_seq[i + 1]] += 1
+
+            # work out the stratigraphic order
+            norm_prob = np.log(strat_mat / np.sum(strat_mat))
+            norm_prob[~np.isfinite(norm_prob)] = 0
+            g = nx.DiGraph(norm_prob)
+
+            # find nodes with no-incoming connections
+            # these are the tops
+            tops = []
+            for n in range(nunits):
+                if g.in_degree(n) == 0:
+                    tops.append(n)
+            # find nodes with no-outgoing connections
+            # these are the bottoms
+            bottoms = []
+            for n in range(nunits):
+                if g.out_degree(n) == 0:
+                    bottoms.append(n)
+            # nodes with a single out-degrees
+            # conformable units
+            # these are the bottoms
+            bottoms = []
+            for n in range(nunits):
+                if g.out_degree(n) == 0:
+                    bottoms.append(n)
+
+            # find the path that visits all the nodes this is the strat column
+            strat_sequence = []
+            for paths in nx.all_simple_paths(g, tops[0], bottoms[0]):
+                if len(paths) == nunits:
+                    strat_sequence = paths
+                    break
+            strat_column = {back_map[s[1]]: s[0] for s in enumerate(strat_sequence)}
+
+        # this calculation does a fractional depth calculation best done for flat lying orebodies
+        # which really is a best guess at getting the stratigraphic order
+        idxfrac = contacts.type == "inside"
+        max_depth = contacts[idxfrac].groupby("feature_name")["val"].max()
+        frac_depth = contacts.val / contacts.feature_name.map(max_depth.to_dict())
+        strat_order = (
+            contacts[idxfrac]
+            .groupby("feature_name")["Z"]
+            .max()
+            .reset_index()
+            .sort_values("Z", ascending=False)
+            .reset_index(drop=True)
+        )
+        # other method of estimating the strat column
+        if use_graph_estimate == False:
+            strat_column = {}
+            for n, i in strat_order.iterrows():
+                strat_column.update({i.feature_name: n})
+        # subtract not add the fractional depth this should give the right answer with respect to interfaces vs internal points
+        contacts.loc[idxfrac, "val"] = (
+            contacts[idxfrac].feature_name.map(strat_column) - frac_depth[idxfrac]
+        )
+        tmdict = {}
+        for i in range(int(contacts.val.min()), int(contacts.val.max()) + 1):
+            tmdict.update({i: {"min": i, "max": i + 1, "id": i}})
+
+        return contacts, tmdict, strat_column
+
+    def to_omf():
+        """
+        generates loop data
         """
         pass
 
     def to_gempy():
         """
-        gempy data
+        generates gempy data
         """
         pass
